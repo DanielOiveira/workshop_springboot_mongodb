7538412b1a7c07a49ca28c52f874de248e3d3078
DanielOiveira <danielcompositor1307@gmail.com>
2021-01-18 05:52:42 -0300

    Projecao dos dados do author com DTO

--- src/main/java/com/nelioalves/workshopmongo/domain/Post.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/Post.java
@@ -6,21 +6,25 @@ import java.util.Date;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.nelioalves.workshopmongo.dto.AuthorDTO;
+
 @Document
-public class Post implements Serializable{
-	
+public class Post implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private Date date;
 	private String title;
 	private String body;
-	private User author;
-	
-	
+	private AuthorDTO author;
 
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post() {
+
+	}
+
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
 		this.date = date;
@@ -28,66 +32,45 @@ public class Post implements Serializable{
 		this.body = body;
 		this.author = author;
 	}
-	
-	
+
 	public String getId() {
 		return id;
 	}
-
-
 
 	public void setId(String id) {
 		this.id = id;
 	}
 
-
-
 	public Date getDate() {
 		return date;
 	}
-
-
 
 	public void setDate(Date date) {
 		this.date = date;
 	}
 
-
-
 	public String getTitle() {
 		return title;
 	}
-
-
 
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
-
-
 	public String getBody() {
 		return body;
 	}
-
-
 
 	public void setBody(String body) {
 		this.body = body;
 	}
 
-
-
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
 
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
-	}
-
-	public Post() {
-		
 	}
 
 	@Override
@@ -114,7 +97,5 @@ public class Post implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
 
 }
