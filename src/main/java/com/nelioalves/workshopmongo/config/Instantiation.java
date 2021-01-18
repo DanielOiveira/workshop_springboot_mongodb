7284da14e4340e22fffeebe1d2d6247c684b1144
DanielOiveira <danielcompositor1307@gmail.com>
2021-01-18 06:07:00 -0300

    Referenciando os posts do usuário

--- src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
+++ src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.nelioalves.workshopmongo.config;
 
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,11 +44,13 @@ public class Instantiation implements CommandLineRunner{
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo",new AuthorDTO(maria) );
 		Post post2 = new Post(null, sdf.parse("25/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
-		
-		
-		
+	
 		postRepository.save(post1);
 		postRepository.save(post2);
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		
+		userRepository.save(maria);
 		
 	}
 
